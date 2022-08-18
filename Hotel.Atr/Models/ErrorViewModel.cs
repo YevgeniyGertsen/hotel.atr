4a2d66b9d3d2833f782ead4e65cca6bc96fe6141
gersen.e.a <gersen.e.a@gmail.com>
2022-08-18 21:23:05 +0600

    Добавьте файлы проекта.

---
+++ Hotel.Atr/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hotel.Atr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
