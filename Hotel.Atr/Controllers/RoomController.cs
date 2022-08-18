b7b145cf092f7dfa5b46e5d83499ab84df8703ea
gersen.e.a <gersen.e.a@gmail.com>
2022-08-18 21:56:02 +0600

    First comit

---
+++ Hotel.Atr/Controllers/RoomController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Atr.Controllers
+{
+    public class RoomController : Controller
+    {
+        // GET: RoomController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RoomList()
+        {
+            return View();
+        }
+
+        public ActionResult RoomDetails()
+        {
+            return View();
+        }
+    }
+}
